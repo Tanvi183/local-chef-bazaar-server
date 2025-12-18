7cb3583c44df064aef0d9a9239244fdef11043c5
tanvi183 <mneshat7@gmail.com>
2025-12-17 23:12:17 -0800

    Connet with mongobd and create mongo client.

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+// use Credentials and Create mongoClient Connect
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@simple-crud-server.7fhuvu7.mongodb.net/?appName=simple-crud-server`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("LocalChef Bazaar is running");
