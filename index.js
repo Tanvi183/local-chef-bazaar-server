df18d6bdf86871c9a173f9c31dba3fa14ba33f12
tanvi183 <mneshat7@gmail.com>
2025-12-17 22:58:16 -0800

    project setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("LocalChef Bazaar is running");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
